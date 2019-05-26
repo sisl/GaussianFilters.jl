d05e12bdc2441356190e67ed7e2c9756520a0dcb
BenQLange <blange@stanford.edu>
2019-05-26 14:39:22 -0700

    added spawn

--- src/classes.jl
+++ src/classes.jl
@@ -1,5 +1,5 @@
 """
-    GaussianMixture(N,w,μ,Σ)
+    GaussianMixture(N, w, μ, Σ)
 
     Arguments:
     N: number of models
@@ -7,10 +7,23 @@
     Σ: Covariances of the model
 """
 
-struct GaussianMixture{N,w,μ,Σ}
+struct GaussianMixture{N, w, μ, Σ}
     N :: Int64
     w :: Vector{Float64}
     μ :: Matrix{Float64,2}
     Σ :: Array{Float64,3}
 end
 
+"""
+    Spawn(β, dyn)
+
+    Arguments:
+    β: Gaussian Mixture model determining the 
+    spawning intesity of the target
+    dyn: Dynamics
+"""
+
+struct Spawn{β, dyn}
+    β :: GaussianMixture
+    dyn :: Dynamics
+end
