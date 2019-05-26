2937729413886b9dd3e589a5c429773ba2edf204
BenQLange <blange@stanford.edu>
2019-05-26 16:15:26 -0700

    fixed classes

--- src/classes.jl
+++ src/classes.jl
@@ -1,3 +1,43 @@
+### Dynamics Model ###
+"""
+    Dynamics(A,Q,d)
+    Dynamics(A,Q)
+
+Construct linear dynamics model with; transition matrix A,
+process noise matrix Q and constant matrix d
+"""
+mutable struct Dynamics
+    A::Matrix{Float64}
+    Q::Matrix{Float64}
+    d::Matrix{Float64}
+end
+
+## Constructors ##
+function Dynamics(A,Q)
+    return Dynamics(A,Q,d = nothing)
+end
+
+function Dynamics(A,Q,d)
+    return Dynamics(A,Q,d)
+end
+
+### Measurement Model ###
+"""
+    Measurement(C,R)
+
+Construct measurement model with observation matrix C and sensor
+noise matrix R
+"""
+mutable struct Measurement
+    C::Matrix{Float64}
+    R::Matrix{Float64}
+end
+
+## Constructor ##
+function Measurement(C,R)
+    return Measurement(C,R)
+end
+
 """
     GaussianMixture(N, w, μ, Σ)
 
@@ -10,8 +50,16 @@
 struct GaussianMixture
     N :: Int64
     w :: Vector{Float64}
-    μ :: Matrix{Float64,2}
+    μ :: Matrix{Float64}
     Σ :: Array{Float64,3}
+end
+
+function GaussianMixture(w,μ,Σ)
+    return GaussianMixture(N = length(w), w, μ, Σ)
+end
+
+function GaussianMixture(N,w,μ,Σ)
+    return GaussianMixture(N,w,μ,Σ)
 end
 
 """
@@ -50,4 +98,3 @@ struct PHDFilter
     Ps :: Float64
     Pd :: Float64
 end
-
