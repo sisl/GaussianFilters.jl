f2137626cf89b3d5626f90f4d55af0d818dd3ba4
Zach <zach@laptop.stanford.edu>
2019-05-31 16:23:06 -0700

    Add 2D example to pruneTest

--- src/pruneTest.jl
+++ src/pruneTest.jl
@@ -4,7 +4,7 @@ include("./plotGMM.jl")
 using Printf
 
 ## Main
-# 1d example
+# 1D example
 n = 1000
 
 w = rand(n)
@@ -27,5 +27,34 @@ title(title_str)
 
 plot1dGMM(x_new, xlim, resolution)
 title_str = @sprintf("After Pruning: N=%d", length(x_new.w))
+title(title_str)
+show()
+
+
+# 2D example
+n = 1000
+
+w = rand(n)
+mu = vcat([randn(2) for i=1:n/2],[randn(2).+ [-5,5] for i=1:n/2])
+sigma = [Matrix(Diagonal(rand(2))) for i=1:n]
+
+T = 0.0			# threshold
+U = 10.0		# clustering threshold
+J_max = 100		# maximum number of points
+
+x = GaussianMixture(w, mu, sigma)
+x_new = prune(x,T,U,J_max)
+
+
+xlim = [-10,10]
+ylim = [-10,10]
+resolution = 100
+
+plot2dGMM(x, xlim, ylim, resolution)
+title_str = @sprintf "Before Pruning: N=%d" n
+title(title_str)
+
+plot2dGMM(x_new, xlim, ylim, resolution)
+title_str = @sprintf "After Pruning: N=%d" length(x_new.w)
 title(title_str)
 show()
