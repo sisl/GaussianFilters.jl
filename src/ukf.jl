fed5cf8a8ffc0ab0b6ec7ae1d210e05d3b5ee533
Arec Jamgochian <jamgochian95@gmail.com>
2019-08-15 00:08:15 -0700

    completing first pass of ukf predict and measure functions

--- src/ukf.jl
+++ src/ukf.jl
@@ -58,64 +58,25 @@ weighting, although this is not necessary.
 
 """
 function unscented_transform_inverse(points::Array{Array{a}}, w_m::Array{b},
-    w_c::Array{c}) where {a<:Number, b<:Number, c<:Number}
+    w_Σ::Array{c}) where {a<:Number, b<:Number, c<:Number}
 
     # calculate weighted mean
     μ = sum(points .* w_μ)
 
     # calculated weighted covariance
     diff = hcat(points...) .- μ
-    scaled = diff .* sqrt.(w_c)'
+    scaled = diff .* sqrt.(w_Σ)'
     Σ = scaled * scaled'
 
     return GaussianBelief(μ, Σ)
 end
-
-#=
-"""
-Unscented Kalman Update
-
-Rework into separate predict and measure functions, with update=predict+measure
-"""
-function UnscentedKalmanUpdate(μ, Σ, u, y, g, h, W, V ;dt=0.001)
-    n = length(μ)
-
-    # predict
-    points, w_m, w_c = UT(μ,Σ)
-    pointsp = zeros(size(points))
-    for i in 1:(2*n+1)
-        pointsp[:,i] = g(points[:,i], u;dt=dt)
-    end
-    μ_pred, Σb_pred = invUT(pointsp, w_m, w_c)
-    Σ_pred = Σb_pred + W
-
-    # update
-    points, w_m, w_c = UT(μ_pred,Σ_pred)
-    y_preds = zeros(length(y),(2*n+1))
-    for i in 1:(2*n+1)
-        y_preds[:,i] = h(points[:,i])
-    end
-    y_pred = y_preds*w_m
-
-    scaled_ydiff = (y_preds .- y_pred).*sqrt.(w_c)'
-    Σ_Y = scaled_ydiff*scaled_ydiff' + V
-
-    scaled_xdiff = (points .- μ_pred).*sqrt.(w_c)'
-    Σ_XY = scaled_xdiff*scaled_ydiff'
-
-    μ = μ_pred + Σ_XY*inv(Σ_Y)*(y-y_pred)
-    Σ = Σ_pred - Σ_XY*inv(Σ_Y)*Σ_XY'
-    Σ = Symmetric(Σ)
-    return μ, Σ
-end
-=#
 
 # Unscented Kalman Filter functions
 
 """
     predict(b0::GaussianBelief, u::Vector, filter::UnscentedKalmanFilter)
 
-Uses Extended Kalman filter to run prediction step on gaussian belief b0,
+Uses Unscented Kalman filter to run prediction step on gaussian belief b0,
 given control vector u.
 """
 function predict(b0::GaussianBelief, u::Vector{a},
@@ -123,34 +84,45 @@ function predict(b0::GaussianBelief, u::Vector{a},
 
     # Motion update
 
-    # Linear motion
-    if filter.d isa LinearDynamicsModel
-        μp = filter.d.A * b0.μ + filter.d.B * u
-        F = filter.d.A
+    n = length(b0.μ)
 
-    # Nonlinear motion
-    elseif filter.d isa NonlinearDynamicsModel
+    # approximate Gaussian belief with sigma points
+    points, w_μ, w_Σ = unscented_transform(b0, filter.λ, filter.α, filter.β)
 
-        # Nonlinear predicted motion
-        μp = filter.d.f(b0.μ, u)
+    # iterate over each sigma point and propagate it through motion function
+    pointsp = []
+    for point in points
 
-        # Nonlinear Jacobian calculated with ForwardDiff.jl
-        F = ForwardDiff.jacobian(μ -> filter.d.f(μ, u), b0.μ)
+        # Linear motion
+        if filter.d isa LinearDynamicsModel
+            push!(pointsp, filter.d.A * point + filter.d.B * u)
 
-    else
-        error("Unsupported EKF Dynamics Model Type: " *
-            string(typeof(filter.d)))
+        # Nonlinear motion
+        elseif filter.d isa NonlinearDynamicsModel
+
+            # Nonlinear predicted motion
+            push!(pointsp, filter.d.f(point, u))
+
+        else
+            error("Unsupported EKF Dynamics Model Type: " *
+                string(typeof(filter.d)))
+        end
     end
 
-    Σp = F * b0.Σ * F' + filter.d.W
-    return GaussianBelief(μp, Σp)
+    # apply inverse unscented transform to approximate new Gaussian
+    bp = unscented_transform_inverse(points_pred, w_μ, w_Σ)
+
+    # add process noise
+    Σp = bp.Σ + filter.d.W
+
+    return GaussianBelief(bp.μ, Σp)
 end
 
 """
     measure(bp::GaussianBelief, y::Vector, filter::UnscentedKalmanFilter;
         u::Vector = [false])
 
-Uses Extended Kalman filter to run measurement update on predicted gaussian
+Uses Unscented Kalman filter to run measurement update on predicted gaussian
 belief bp, given measurement vector y. If u is specified and filter.o.D has
 been declared, then matrix D will be factored into the y predictions.
 """
@@ -159,42 +131,55 @@ function measure(bp::GaussianBelief, y::Vector{a}, filter::UnscentedKalmanFilter
 
     # Measurement update
 
-    # Linear measurement
-    if filter.o isa LinearObservationModel
+    # approximate Gaussian belief with sigma points
+    points, w_μ, w_Σ = unscented_transform(b0, filter.λ, filter.α, filter.β)
 
-        # Linear predicted measurement
-        yp = filter.o.C * bp.μ
-        if !(filter.o.D[1,1] isa Bool)
-            if u[1]==false
-                @warn "D matrix specified in measurement model but not being used"
-            else
-                yp = yp + filter.o.D * u
+    # iterate over sigma points and computed expected measurement
+    ysp = []
+    for point in points
+
+        # Linear measurement
+        if filter.o isa LinearObservationModel
+
+            # Linear predicted measurement
+            yp = filter.o.C * point
+            if !(filter.o.D[1,1] isa Bool)
+                if u[1]==false
+                    @warn "D matrix specified in measurement model but not being used"
+                else
+                    yp = yp + filter.o.D * u
+                end
             end
+            push!(ysp, yp)
+
+        # Nonlinear measurement
+        elseif filter.o isa NonlinearObservationModel
+
+            # Nonlinear predicted measurement
+            push!(ysp,filter.o.h(point, u))
+
+        else
+            error("Unsupported EKF Observation Model Type: " *
+                string(typeof(filter.o)))
         end
 
-        # Linear Jacobian
-        H = filter.o.C
-
-    # Nonlinear measurement
-    elseif filter.o isa NonlinearObservationModel
-
-        # Nonlinear predicted measurement
-        yp = filter.o.h(bp.μ, u)
-
-        # Nonlinear Jacobian calculated with ForwardDiff.jl
-        H = ForwardDiff.jacobian(μ -> filter.o.h(μ, u), bp.μ)
-
-    else
-        error("Unsupported EKF Observation Model Type: " *
-            string(typeof(filter.o)))
     end
 
-    # Kalman Gain
-    K = bp.Σ * H' * inv(H * bp.Σ * H' + filter.o.V)
+    # compute expected measurement
+    yp = sum(ysp .* w_μ)
 
-    # Measurement update
-    μn = bp.μ + K * (y - yp)
-    Σn = (I - K * H) * bp.Σ
+    # compute marginal covariance components
+    ydiff = hcat(ysp...) .- yp
+    scaled_ydiff = ydiff .* sqrt.(w_Σ)'
+    Σ_Y = scaled_ydiff * scaled_ydiff' + filter.o.V
+
+    xdiff = hcat(points...) .- bp.μ
+    scaled_xdiff = xdiff .* sqrt.(w_Σ)'
+    Σ_XY = scaled_xdiff * scaled_ydiff'
+
+    # measurement update
+    μn = bp.μ + Σ_XY * inv(Σ_Y) * (y - y_pred)
+    Σn = bp.Σ - Σ_XY * inv(Σ_Y) * Σ_XY'
     return GaussianBelief(μn, Σn)
 end
 
