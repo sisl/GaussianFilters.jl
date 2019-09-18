d294e9bc2229cec2a563e8bf9e8bbc00ee059908
Arec Jamgochian <jamgochian95@gmail.com>
2019-09-18 15:12:22 -0700

    adding tests for kalman filter functionality

---
+++ test/dummy.jl
@@ -0,0 +1,80 @@
+include("runtests.jl")
+
+
+@time @testset "New Dummy Testing" begin
+
+    Random.seed!(2019)
+
+    # Dynamics
+    dt=0.1
+    function dynamics(x,u)
+        v,ϕ = u
+        xp = x
+        xp += dt*([v*cos(x[3]);v*sin(x[3]); ϕ])
+        xp[3] = xp[3]
+        return xp
+    end
+
+    W = dt*0.05*Matrix{Float64}(I,3,3)
+
+    dmodel = NonlinearDynamicsModel(dynamics,W)
+
+    # Measurement
+    observe(x,u) = [norm(x[1:2])]
+
+    V = 0.1*Matrix{Float64}(I,1,1)
+
+    omodel = NonlinearObservationModel(observe,V)
+
+    # Build UKF
+    ukf = UnscentedKalmanFilter(dmodel,omodel);
+
+    # Simulation
+    times = 0:dt:5
+    action_sequence = [[1.0, 2*sin(pi/2*t)] for t in times]
+
+    b0 = GaussianBelief([0.0,0.0,0.0], 0.01*Matrix{Float64}(I,3,3))
+
+    sim_states, sim_measurements = simulation(b0,action_sequence,ukf);
+
+    # Filter
+    filtered_beliefs = run_filter(b0, action_sequence, sim_measurements, ukf)
+    μ, Σ = unpack(filtered_beliefs)
+
+    # Tests
+    @test ukf.d isa NonlinearDynamicsModel
+    @test ukf.o isa NonlinearObservationModel
+    @test length(sim_measurements) == length(action_sequence)
+
+
+    first_update = update(b0, action_sequence[1], sim_measurements[1], ukf)
+    first_predict = predict(b0, action_sequence[1], ukf)
+    first_predict_measure = measure(first_predict, sim_measurements[1], ukf,
+                            u = action_sequence[1])
+    array_isapprox(first_update.μ, first_predict_measure.μ)
+    array_isapprox(first_update.Σ, first_predict_measure.Σ)
+
+
+    @test length(filtered_beliefs[end].μ) == length(b0.μ)
+    @test size(filtered_beliefs[end].Σ) == size(b0.Σ)
+    @test size(μ) == (length(filtered_beliefs), length(filtered_beliefs[1].μ))
+    @test size(Σ) == (length(filtered_beliefs), size(filtered_beliefs[1].Σ)...)
+
+    # change with algorithm / rng changes
+
+    array_isapprox(sim_states[end], [0.16459, 3.34923, 2.65721];atol=0.001)
+    array_isapprox(sim_measurements[end], [3.17086];atol=0.001)
+    array_isapprox(filtered_beliefs[end].μ, [1.83418, 2.78963, 1.37579];
+                    atol=0.001)
+    array_isapprox(filtered_beliefs[end].Σ, [0.739291 -0.472135 -0.282821;
+                                            -0.472135 0.342115 0.197056;
+                                            -0.282821 0.197056 0.185713]; atol=0.001)
+
+end
+
+#=
+@show sim_states[end]
+@show sim_measurements[end]
+@show filtered_beliefs[end].μ
+@show filtered_beliefs[end].Σ
+=#
---
+++ test/test_kf.jl
@@ -0,0 +1,58 @@
+Random.seed!(0)
+
+# Dynamics
+dt = 0.1
+m = 50
+A = [1 dt 0 0 ; 0 1 0 0 ; 0 0 1 dt; 0 0 0 1]
+B = [0 0; dt/m 0; 0 0; 0 dt/m]
+W = 0.1*Matrix{Float64}(I,4,4)
+
+dmodel = LinearDynamicsModel(A,B,W);
+
+# Measurement
+C = [0 1.0 0 0; 0 0 0 1.0]
+V = 0.5*Matrix{Float64}(I,2,2)
+
+omodel = LinearObservationModel(C,V)
+
+# Build KF
+kf = KalmanFilter(dmodel,omodel)
+
+# Simulation
+b0 = GaussianBelief([0.0,0.0,0.0,0.0], 2.0*Matrix{Float64}(I,4,4))
+
+times = 0:dt:10
+Fmag = 1000
+action_sequence = [[Fmag*cos(t), Fmag*sin(t)] for t in times]
+
+sim_states, sim_measurements = simulation(b0,action_sequence,kf);
+
+# Filter
+filtered_beliefs = run_filter(b0, action_sequence, sim_measurements, kf)
+μ, Σ = unpack(filtered_beliefs);
+
+# Tests
+@test kf.d isa LinearDynamicsModel
+@test kf.o isa LinearObservationModel
+@test length(sim_measurements) == length(action_sequence)
+
+first_update = update(b0, action_sequence[1], sim_measurements[1], kf)
+first_predict_measure = measure(predict(b0, action_sequence[1], kf),
+    sim_measurements[1], kf; u = action_sequence[1])
+array_isapprox(first_update.μ, first_predict_measure.μ)
+array_isapprox(first_update.Σ, first_predict_measure.Σ)
+
+@test length(filtered_beliefs[end].μ) == length(b0.μ)
+@test size(filtered_beliefs[end].Σ) == size(b0.Σ)
+@test size(μ) == (length(filtered_beliefs), length(filtered_beliefs[1].μ))
+@test size(Σ) == (length(filtered_beliefs), size(filtered_beliefs[1].Σ)...)
+
+# change with algorithm / rng changes
+array_isapprox(sim_states[end], [59.9223, -7.17527, 178.149, 30.4653];atol=0.001)
+array_isapprox(sim_measurements[end], [-7.01552, 30.4259];atol=0.001)
+array_isapprox(filtered_beliefs[end].μ, [60.1627, -7.13376, 183.227, 30.3895];
+                atol=0.001)
+array_isapprox(filtered_beliefs[end].Σ,   [12.6069 0.0320871 0.0 0.0;
+                                            0.0320871 0.179129 0.0 0.0;
+                                            0.0 0.0 12.6069 0.0320871;
+                                            0.0 0.0 0.0320871 0.179129]; atol=0.001)
